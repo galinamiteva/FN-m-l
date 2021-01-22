2185fa8999475045de9eb3f071ba073dea2a7023
galinamiteva <70331948+galinamiteva@users.noreply.github.com>
2021-01-22 12:53:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -28,69 +28,15 @@ function App(props){
                 </div>
             )
         }
-        import React, {useState, useEffect} from 'react';
+      
 
-
-        import './App.css';
-        import Goal from './Goal';
-        
-        
-        
-        function App(props){
-            const[currentGoal, setCurrentGoal] = useState([]);
-            
-        
-             useEffect(async () => {
-                const response = await fetch('https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false');
-                const data = await response.json();  
-                
-                setCurrentGoal(data);
-                
-            }, []); 
-          
-          
-                function GeneralView({goal, moreInfo}){
-                    return(
-                        <div className='goal'>
-                            <p>{goal.title}</p>
-                            <span> {goal.code}</span>
-                            <button data-code={goal.code} onClick={()=>moreInfo(goal)}>Läs mer</button>
-                        </div>
-                    )
-                }
-              
-        
-        return (    
-            <main>
-                <section className="logo">
-                    <h1>FN:s hållbarhetsmål</h1>
-                </section>
-        
-                    {currentGoal? <Goal goal={currentGoal} close={setCurrentGoal}/>: 
-        
-                <section id="goals">
-                    {currentGoal.map((goal, index)=>{   
-                        return <GeneralView goal={goal} moreInfo={setCurrentGoal} key={index}/> 
-                        
-                    })}
-                </section>
-                }
-            </main>
-        )
-        }
-        
-        export default App;
-        
-    
-
- 
 return (    
     <main>
         <section className="logo">
 	        <h1>FN:s hållbarhetsmål</h1>
         </section>
 
-            {currentGoal? <Goal goal={currentGoal} close={setCurrentGoal} />: 
+            {currentGoal? <Goal goal={currentGoal} close={setCurrentGoal}/>: 
 
         <section id="goals">
             {currentGoal.map((goal, index)=>{   
--- src/Goal.js
+++ src/Goal.js
@@ -1,31 +1,52 @@
-function dataGoalsCode(){
-    {dataGoals.map((dataGoal)=>{
-        <h1 className='overlay-info-header'>`Goal ${dataGoal.code}`</h1>
-    })
-}}
+import React, {useState, useEffect} from 'react';
 
-function dataGoalsdescription (){
-    {dataGoals.map(()=>{
-        <p className='summary'>{dataGoals.description}</p>
-    })}        
-}
+import './Goal.css';
 
-return(<section className='overlay show'>
-        {dataGoals.targets.map(( target, index)=>{
-            return <div className='overlay-info' key={index}>
-                        {dataGoalsCode}
-                        <h2 className='overlay-info-title'>{target.title}</h2>
-                        <article id='info-text'>
-                                {dataGoalsdescription}
-                                <p> {target.description}</p>
-                        </article>
-                        <button id='info-close'  onClick={()=> close('')}>X</button>
-                       
-                    </div>
-        })}                          
-            
-    </section>
-)
+
+
+function Goal({goal, close}){
+    const goalNumber=goal.code;    // Här jag vill hämta goalNumber från den andra API:n för att använda i fectchen ner, men kan inte !
+   
+    const[dataGoals, setDataGoal] = useState([]);
+    
+    useEffect(async () => {
+        const response = await fetch(`https://unstats.un.org/SDGAPI/v1/sdg/Goal/${goalNumber}/Target/List?includechildren=true`);
+        const data = await response.json();
+        
+        setDataGoal(data);        
+        
+                
+    }, []);
+
+   
+    function dataGoalsCode(){
+        {dataGoals.map((dataGoal)=>{
+            <h1 className='overlay-info-header'>`Goal ${dataGoal.code}`</h1>
+        })
+    }}
+   
+    function dataGoalsdescription (){
+        {dataGoals.map(()=>{
+            <p className='summary'>{dataGoals.description}</p>
+        })}        
+    }
+
+    return(<section className='overlay show'>
+            {dataGoals.targets.map(( target, index)=>{
+                return <div className='overlay-info' key={index}>
+                            {dataGoalsCode}
+                            <h2 className='overlay-info-title'>{target.title}</h2>
+                            <article id='info-text'>
+                                    {dataGoalsdescription}
+                                    <p> {target.description}</p>
+                            </article>
+                            <button id='info-close'  onClick={()=> close('')}>X</button>
+                           
+                        </div>
+            })}                          
+                
+        </section>
+    )
 
 }
 
